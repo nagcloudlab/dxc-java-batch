c6385128ff6fc5f247dc3e4ea52f5dc4108b4d95
nag <nagtraininglab@gmail.com>
2023-08-09 17:53:54 +0530

    java collections

---
+++ collections/src/main/java/com/example/Box.java
@@ -0,0 +1,16 @@
+package com.example;
+
+
+public class Box<E> {
+    Object[] items = new Object[10];
+    int index = -1;
+
+    public void addItem(E item) {
+        index++;
+        items[index] = item;
+    }
+
+    public E getItem(int index) {
+        return (E) items[index];
+    }
+}
